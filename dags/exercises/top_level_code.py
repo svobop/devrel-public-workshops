8fc106bf5b33a83fa36dc8968fdc0f7d5376efc7
Ryan Hatter <ryanhatter10@gmail.com>
2024-09-06 16:33:45 -0400

    add docs for the exercise

--- dags/exercises/top_level_code.py
+++ dags/exercises/top_level_code.py
@@ -2,17 +2,7 @@ from datetime import datetime
 
 from airflow.decorators import dag, task
 
-from time import sleep
-
-# from include.helper_functions import expensive_api_call
-
-
-def expensive_api_call():
-    """
-    Returns the answer to the question "What is the meaning of life, the universe, and everything?"
-    """
-    sleep(40)  # sleeping for 100 seconds simulates an expensive API call
-    return 42
+from include.helper_functions import expensive_api_call
 
 
 @dag(start_date=datetime(2023, 1, 1), max_active_runs=3, schedule=None, catchup=False)
